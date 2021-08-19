ed8e6c77d675487ea83c6e905c59e2c914549e5f
Dave Bloom <dave@digitalnoise.net>
2021-08-19 11:24:27 -0700

    First the node_modules needs to exist

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ TIMESTAMP=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 SRC_DIR=$1; shift
 ESLINT_OPTS=$@
 
+cd /github/workspace/$SRC_DIR
+npm i
+
 cd /github/workspace
 /usr/bin/eslint -f @microsoft/eslint-formatter-sarif -o eslint.sarif --resolve-plugins-relative-to /github/workspace/$SRC_DIR $ESLINT_OPTS $SRC_DIR
 
