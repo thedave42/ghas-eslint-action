f210ec93090baa8da82714b71115b2087b8d26e5
Dave Bloom <dave@digitalnoise.net>
2021-08-23 18:06:56 -0700

    Script fixes

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ AUTOMATION_ID="$SARIF_CATEGORY$GITHUB_RUN_ID"
 cd /github/workspace/$SRC_DIR
 /usr/bin/eslint -f @microsoft/eslint-formatter-sarif -o $GITHUB_RUN_ID.sarif $ESLINT_OPTS . || exit 0
 jq --arg a $AUTOMATION_ID '.runs[0] |= . + {"automationDetails": {"id": $a}}' $GITHUB_RUN_ID.sarif > $GITHUB_JOB.sarif
-if [ $? -ne 0 ]
+if [ ! $? -eq 0 ]
 then
   echo Failed with error code $?
   exit $?
